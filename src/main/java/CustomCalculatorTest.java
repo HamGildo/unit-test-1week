28cbfe6c405396240fda8d16eb897787266205d7
HamGildo <ham5477@naver.com>
2019-04-15 23:46:17 +0900

    Submit realCoding_unit-test1

---
+++ src/main/java/CustomCalculatorTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomCalculatorTest {
+
+    @Test
+    public void 더하기테스트() {
+        CustomCalculator customCalculator = new CustomCalculator();
+        int result = customCalculator.add(3,4);
+
+        assertTrue( result == 7);
+    }
+
+    @Test
+    public void 빼기테스트() {
+        CustomCalculator customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(10,2);
+        assertTrue(result == 8);
+    }
+
+    @Test
+    public void 곱하기테스트() {
+        CustomCalculator customCalculator = new CustomCalculator();
+        int result = customCalculator.multiply(10,2);
+        assertTrue(result == 20);
+    }
+
+    @Test
+    public void 나누기테스트() {
+        CustomCalculator customCalculator = new CustomCalculator();
+        int result = customCalculator.divide(10,2);
+        assertTrue(result == 5);
+    }
+
+
+}
